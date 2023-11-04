343eca3cc1127286a751a1bd9c175c4fc67480ab
paritPranav <paritpranav@gmail.com>
2023-11-04 19:47:06 +0530

    added src lattitude,longitude in rides model

--- models/RideList.js
+++ models/RideList.js
@@ -3,11 +3,19 @@ const Driver=require('./Driver');
 const Passanger=require('./Passanger');
 
 const RideSchema = new mongoose.Schema({
-  source: {
+  source_latitude: {
     type: String,
     required: true,
   },
-  destination: {
+  destination_latitude: {
+    type: String,
+    required: true,
+  },
+  source_longitude: {
+    type: String,
+    required: true,
+  },
+  destination_longitude: {
     type: String,
     required: true,
   },
