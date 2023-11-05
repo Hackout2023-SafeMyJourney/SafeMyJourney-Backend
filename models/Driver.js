7a7fe529ceccc8e80d29d4bca1b6863f3b37f9a5
atharvaagrawal <atharva7199990@gmail.com>
2023-11-05 10:13:42 +0530

    GitIgnore Updated

---
+++ models/Driver.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const DriverSchema = new mongoose.Schema({
+  Name: {
+    type: String,
+    required: true,
+  },
+  Mobile: {
+    type: String,
+    required: true,
+  },
+  Age: {
+    type: String,
+    required: true,
+  },
+  Gender: {
+    type: String,
+    required: true,
+  },
+  Email_Id: {
+    type: String,
+    required: true,
+  },
+  Password: {
+    type: String,
+    required: true,
+  },
+  Cab_No:{
+    type:String,
+    required:true,
+  },
+  Aadhar_No:{
+    type:String,
+    required:true,
+  }
+});
+
+module.exports = mongoose.model("Driver", DriverSchema);
---
+++ routes/PassangerJwtVerify.js
@@ -0,0 +1,28 @@
+const jwt =require('jsonwebtoken');
+const { ensureIndexes } = require('../models/Passanger');
+require('dotenv/config');
+const User = require("../models/Passanger");
+
+const { CommentsController } = require("moongose/controller");
+
+// const app=express();
+
+module.exports = async (req,res,next)=>{
+
+try{
+
+    const {authtoken}= req.headers;
+    if(!authtoken){
+        return next("Please enter the login access data");
+    }const verify= await jwt.verify(authtoken,process.env.TOKEN_SECRET);
+    req.user =await User.findById(verify.id);
+    next();
+
+
+}catch(err){
+    return next(err);
+
+}
+
+
+}
