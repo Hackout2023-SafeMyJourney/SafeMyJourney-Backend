e2cb8cae9baa06c9345cdc8040ce6cf2b6aa845a
atharvaagrawal <atharva7199990@gmail.com>
2023-11-05 10:02:45 +0530

    Added Readme

--- routes/Driver.js
+++ routes/Driver.js
@@ -1,123 +1,99 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const Driver=require('../models/Driver');
-const jwt = require('jsonwebtoken');
-const verify=require('./DriverJwtVerify');
-const multer =require('multer');
+const Driver = require("../models/Driver");
+const jwt = require("jsonwebtoken");
+const verify = require("./DriverJwtVerify");
+const multer = require("multer");
 const formidable = require("formidable");
-const  fs=require('fs');
-const util=require('util')
-const unlinkFile=util.promisify(fs.unlink)
- 
-const path = require('path');
+const fs = require("fs");
+const util = require("util");
+const unlinkFile = util.promisify(fs.unlink);
+
+const path = require("path");
 var cookies = require("cookie-parser");
 
-var ObjectId = require('mongodb').ObjectID;
+var ObjectId = require("mongodb").ObjectID;
 
+const { uploadFile } = require("./s3");
 
-const {uploadFile}=require("./s3")
+const app = express();
 
+app.use(express.static("public"));
 
-
-const app=express();
-
-app.use(express.static("public"))
-
-    
 app.use(bodyParser.json());
 
 app.use(cookies());
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 const router = express.Router();
-const upload=multer({dest:'uploads/'});
+const upload = multer({ dest: "uploads/" });
 
-
-router.get('/',(req,res)=>{
-    res.send("Driver")
-})
-
+router.get("/", (req, res) => {
+  res.send("Driver");
+});
 
 //get all drivers
 
 //get specific driver details
-router.get("")
+router.get("");
 //update driver details
 
-router.get("/getdriver",async(req,res)=>{
-    console.log(req.body.id);
+router.get("/getdriver", async (req, res) => {
+  console.log(req.body.id);
 
-    var DriverDetails=await Driver.findById(req.body.id);
-    console.log(DriverDetails)
-    res.send(DriverDetails)
-    
-
-
-})
+  var DriverDetails = await Driver.findById(req.body.id);
+  console.log(DriverDetails);
+  res.send(DriverDetails);
+});
 
 //New registreation ;
 router.post("/register", async (req, res) => {
+  const newDriver = new Driver({
+    Name: req.body.name,
+    Mobile: req.body.mobile,
+    Age: req.body.age,
+    Gender: req.body.gender,
+    Email_Id: req.body.email,
+    Cab_No: req.body.cab_no,
+    Aadhar_No: req.body.aadhar_no,
+    Password: req.body.password,
+  });
 
-    const newDriver = new Driver({
-            Name: req.body.name,
-            Mobile:req.body.mobile,
-            Age: req.body.age,
-            Gender:req.body.gender,
-            Email_Id:req.body.email,
-            Cab_No:req.body.cab_no,
-            Aadhar_No:req.body.aadhar_no,
-            Password:req.body.password
-        })
-        
-        try{
-            const savedPost= await newDriver.save();
+  try {
+    const savedPost = await newDriver.save();
 
-            res.send({ message: "Success", success: true });
-            res.status(200)
-      }catch(err){
-       res.send(err);
-      }
- 
+    res.send({ message: "Success", success: true });
+    res.status(200);
+  } catch (err) {
+    res.send(err);
+  }
 });
-
 
 //Login
 
-router.post("/login",async(req,res)=>{
-
-const idExist = await Driver.findOne({
+router.post("/login", async (req, res) => {
+  const idExist = await Driver.findOne({
     Email_Id: req.body.id,
     Password: req.body.pass,
+  });
+  try {
+    if (!idExist) {
+      return res.status(301).send({ message: "not-loggedin", success: false });
+    } else {
+      const token = await jwt.sign(
+        { id: idExist._id },
+        process.env.TOKEN_SECRET,
+        {
+          expiresIn: process.env.JWT_EXPIRE,
+        }
+      );
+
+      res.send({ token });
+      res.status(200);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
-try{
 
-if (!idExist){
-    return res.status(301).send({ message: "not-loggedin", success: false });
-}else{
-    
-const token = await jwt.sign({ id: idExist._id }, process.env.TOKEN_SECRET,{
-
-    expiresIn:process.env.JWT_EXPIRE,
-
-});
-
-res.send({token});
-res.status(200); 
-}
-
-}catch (err){
-        console.log(err);
-}
-
-})
-
-
-
-
-
-
-
-
-
-
- module.exports = router;
+module.exports = router;
--- routes/Passanger.js
+++ routes/Passanger.js
@@ -1,121 +1,99 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const Passanger=require('../models/Passanger');
-const jwt = require('jsonwebtoken');
-const verify=require('./PassangerJwtVerify');
-const multer =require('multer');
+const Passanger = require("../models/Passanger");
+const jwt = require("jsonwebtoken");
+const verify = require("./PassangerJwtVerify");
+const multer = require("multer");
 const formidable = require("formidable");
-const  fs=require('fs');
-const util=require('util')
-const unlinkFile=util.promisify(fs.unlink)
- 
-const path = require('path');
+const fs = require("fs");
+const util = require("util");
+const unlinkFile = util.promisify(fs.unlink);
+
+const path = require("path");
 var cookies = require("cookie-parser");
 
-var ObjectId = require('mongodb').ObjectID;
+var ObjectId = require("mongodb").ObjectID;
 
+const { uploadFile } = require("./s3");
 
-const {uploadFile}=require("./s3")
+const app = express();
 
+app.use(express.static("public"));
 
-
-const app=express();
-
-app.use(express.static("public"))
-
-    
 app.use(bodyParser.json());
 
 app.use(cookies());
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 const router = express.Router();
-const upload=multer({dest:'uploads/'});
+const upload = multer({ dest: "uploads/" });
 
-
-
-router.get('/',(req,res)=>{
-    res.send("passasnger")
-})
-
+router.get("/", (req, res) => {
+  res.send("passasnger");
+});
 
 //get all passangers
 
 //get specific passanger details
-router.get("/getpassanger",async(req,res)=>{
-    console.log(req.body.id);
+router.get("/getpassanger", async (req, res) => {
+  console.log(req.body.id);
 
-    var PassangerDetails=await Passanger.findById(req.body.id);
-    console.log(PassangerDetails)
-    res.send(PassangerDetails)
-    
-
-
-})
+  var PassangerDetails = await Passanger.findById(req.body.id);
+  console.log(PassangerDetails);
+  res.send(PassangerDetails);
+});
 
 //update passanger details
 
-
-
 //New registreation ;
 router.post("/register", async (req, res) => {
+  const newPassanger = new Passanger({
+    Name: req.body.name,
+    Mobile: req.body.mobile,
+    Age: req.body.age,
+    Gender: req.body.gender,
+    Email_Id: req.body.email,
+    nomini_name: req.body.nomini,
+    nomini_Mobile: req.body.nominiMobile,
+    Password: req.body.password,
+  });
 
-		const newPassanger = new Passanger({
-                Name: req.body.name,
-                Mobile:req.body.mobile,
-                Age: req.body.age,
-                Gender:req.body.gender,
-                Email_Id:req.body.email,
-                nomini_name:req.body.nomini,
-                nomini_Mobile:req.body.nominiMobile,
-                Password:req.body.password
-            })
-            
-            try{
-                const savedPost= await newPassanger.save();
-   
+  try {
+    const savedPost = await newPassanger.save();
 
-                res.send({ message: "Success", success: true });
-                res.status(200)
-          }catch(err){
-           res.send(err);
-           res.status(301);
-          }
-     
+    res.send({ message: "Success", success: true });
+    res.status(200);
+  } catch (err) {
+    res.send(err);
+    res.status(301);
+  }
 });
-
 
 //Login
 
-router.post("/login",async(req,res)=>{
+router.post("/login", async (req, res) => {
+  const idExist = await Passanger.findOne({
+    Email_Id: req.body.id,
+    Password: req.body.pass,
+  });
+  try {
+    if (!idExist) {
+      return res.status(301).send({ message: "not-loggedin", success: false });
+    } else {
+      const token = await jwt.sign(
+        { id: Passanger._id },
+        process.env.TOKEN_SECRET,
+        {
+          expiresIn: process.env.JWT_EXPIRE,
+        }
+      );
 
-    const idExist = await Passanger.findOne({
-        Email_Id: req.body.id,
-        Password: req.body.pass,
-    });
-    try{
-    
-    if (!idExist){
-        return res.status(301).send({ message: "not-loggedin", success: false });
-    }else{
-        
-    const token = await jwt.sign({ id: Passanger._id }, process.env.TOKEN_SECRET,{
-
-        expiresIn:process.env.JWT_EXPIRE,
-
-    });
-
-    res.send({token});
-    res.status(200);	
-}
-
-    }catch (err){
-            console.log(err);
+      res.send({ token });
+      res.status(200);
     }
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-})
-
-
-
-
- module.exports = router;
+module.exports = router;
